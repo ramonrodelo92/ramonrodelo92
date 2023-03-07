929ff69ba2236746b6d261257a205c0d5e9af685
Ramon Rodelo <ramon.rodeloc@gmail.com>
2023-03-07 11:10:29 -0700

    creando el proyecto con maven

---
+++ coppel/src/test/java/com/testbackend/coppel/CoppelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testbackend.coppel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoppelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
